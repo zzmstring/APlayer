eb9ae86e3ab3e67deac3820cb4f283df7cc1ce50
zzmstring <ac8512@126.com>
2015-03-05 19:00:08 +0800

    add hawk

---
+++ aoobar/src/com/zzmstring/aoobar/support/hawk/LogLevel.java
@@ -0,0 +1,16 @@
+package com.zzmstring.aoobar.support.hawk;
+
+/**
+ * @author Orhan Obut
+ */
+public enum LogLevel {
+    /**
+     * No logs are printed
+     */
+    NONE,
+
+    /**
+     * Print all logs
+     */
+    FULL
+}
