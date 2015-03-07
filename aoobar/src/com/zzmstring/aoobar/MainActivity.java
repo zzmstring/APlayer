23185c908c13f0f30828f9cafd6ab24391908a0a
zzmstring <ac8512@126.com>
2015-03-07 22:12:07 +0800

    3/7add

--- aoobar/src/com/zzmstring/aoobar/MainActivity.java
+++ aoobar/src/com/zzmstring/aoobar/MainActivity.java
@@ -1,10 +1,16 @@
 package com.zzmstring.aoobar;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.lidroid.xutils.ViewUtils;
@@ -17,11 +23,24 @@ import java.util.Observable;
 public class MainActivity extends FragmentActivity implements View.OnClickListener {
     @ViewInject(R.id.tabs)
     PagerSlidingTabStrip tabs;
-    @ViewInject(R.id.tv_addfragment)
-    TextView tv_addfragment;
+    @ViewInject(R.id.iv_addfragment)
+    ImageView iv_addfragment;
     @ViewInject(R.id.view_pager)
     ViewPager view_pager;
-	@Override
+    @ViewInject(R.id.activity_main_tv_artist)
+    TextView activity_main_tv_artist;
+    @ViewInject(R.id.activity_main_tv_name)
+    TextView activity_main_tv_name;
+    @ViewInject(R.id.activity_main_tv_time)
+    TextView activity_main_tv_time;
+    @ViewInject(R.id.activity_main_ib_previous)
+    ImageButton activity_main_ib_previous;
+    @ViewInject(R.id.activity_main_ib_play)
+    ImageButton activity_main_ib_play;
+    @ViewInject(R.id.activity_main_ib_next)
+    ImageButton activity_main_ib_next;
+    private AlertDialog dialog;
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         initView();
@@ -37,17 +56,49 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
     }
     protected void initListener(){
-        tv_addfragment.setOnClickListener(this);
-
-
+        iv_addfragment.setOnClickListener(this);
+        activity_main_ib_previous.setOnClickListener(this);
+        activity_main_ib_play.setOnClickListener(this);
+        activity_main_ib_next.setOnClickListener(this);
     }
-
-
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.tv_addfragment:
+            case R.id.iv_addfragment:
+                showAddTitle();
                 break;
+            case R.id.activity_main_ib_next:
+                break;
+            case R.id.activity_main_ib_play:
+                break;
+            case R.id.activity_main_ib_previous:
+                break;
+
         }
+    }
+    private void showAddTitle(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View v2 = View.inflate(this, R.layout.pupop_edit, null);
+        Button ok = (Button) v2.findViewById(R.id.bt_ok);
+        Button cancel = (Button) v2.findViewById(R.id.bt_cancel);
+        final EditText et_edit=(EditText)v2.findViewById(R.id.et_edit);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content=et_edit.getText().toString().trim();
+                if(!TextUtils.isEmpty(content)){
+
+                }
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog = builder.create();
+        dialog.setView(v2, 0, 0, 0, 0);
+        dialog.show();
     }
 }
---
+++ aoobar/src/com/zzmstring/aoobar/base/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.zzmstring.aoobar.base;
+
+import android.app.Application;
+
+/**
+ * Created by zzmstring on 2015/3/7.
+ */
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
